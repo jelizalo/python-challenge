6599ccd631a55acf1af7b2a0a0c6071410aac56e
Jasmine <jasmineelizalo@gmail.com>
2018-02-06 13:16:45 -0600

    in progress

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,36 +9,36 @@ with open (csvpath1, 'r', newline='') as csvfile:
 
 	#defining the variables
 	votes = 0
-	candidates = []
+	candidates = {}
 	for row in csvreader:
 		#finding total number of votes
-		#votes = votes + 1
+		votes = votes + 1
+
+		#Create a dictionary of the candiate with the number of vote
+		if row[2] not in candidate.keys():
+			num_of_elections = 0
+			candidates.update({row[2]:num_of_elections})
+		if row[2] in candidates.keys():
+			candidates[row[2]] +=1
+		#store each candidate's election number
+			for candidate, vote in elections.items():
+		print(candidate + ":" + vote ) 
+	#print(candidates)
+
  
-    	if row[2] not in candidates:
-           candidates = row[2]
-           candidates.append(row[2]):
-    print(candidates)
 
-	#print("Election Results")
-	#print("---------------------------")
-	#print("Total Votes: " + str(votes))
-	#print("---------------------------")
-	#print()
-
-   	#candidates = []
-    #election_data_1 = list(election_data_1)
-    #print(len(election_data_1))
-   	#for row in election_data_1: 
-    	#if row[2] not in candidates:
-           #candidates.append(row[2])
-    #print(candidates)
-# Create a dictionary of the candiate with the number of vote
-    #candidate = {}
+    
+   # candidate = {}
     #number_of_election = 0
-    #for row in election_data_1: 
-        #if row[2] not in candidate.keys():
-            #number_of_election = 0
-            #candidate.update({row[2]: number_of_election})
+    #for row in csvreader: 
+      #  if row[2] not in candidate.keys():
+       #     candidate.update({row[2]: number_of_election})
         #if row[2] in candidate.keys(): 
-           # candidate[row[2]] += 1
+        #	candidate[row[2]] += 1
     #print(candidate)
+
+#print("Election Results")
+#print("---------------------------")
+#print("Total Votes: " + str(votes))
+#print("---------------------------")
+#print(str{candidate})
